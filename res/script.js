4f50ecfcdac6e1532960d7f5713f65afc2f0d4ef
arturtp <83133969+arturtp@users.noreply.github.com>
2021-06-22 11:12:14 -0300

    Add files via upload

---
+++ res/script.js
@@ -0,0 +1,16 @@
+function xadrez(){
+
+var img = window.document.getElementById('imagem')
+img.scr = 'puzzle.jpg'
+
+var chute = window.document.getElementById('lance')
+var resposta = document.getElementById('resposta')
+
+
+if (chute == "Dd1"){
+    resposta.innerHTML = 'Acertou!'
+} else {
+    resposta.innerHTML = 'Errou!'
+}
+
+}
